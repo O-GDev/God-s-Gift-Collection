ac1889823e4aaba9b5a21aa332586d927d401456
oluwagbogo <omiwoleoluwagbogo@gmail.com>
2023-06-24 15:44:37 +0100

    Last Commit

---
+++ main.py
@@ -0,0 +1,3 @@
+from ecommerce.wsgi import application                                                             
+if __name__ == "__main__":                                             
+    application.run(debug=True)
